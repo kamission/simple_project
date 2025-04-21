32f671b9b71b002ad1c713225d215d62b8c67a2f
kamission <kamission@gmail.com>
2025-04-21 20:35:42 +0300

    added function say_hello

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+def say_hello(name):
+    print(f"Hello {name.upper()}")
+
+
 def main():
     print("Hello world!!!")
+
     if 5 == 5:
         print(True)
+
     for i in range(1, 6):
         if i == 2:
             print("Python!")
+
+    say_hello("Inna")
+    say_hello("Borys")
 
 
 if __name__ == '__main__':
